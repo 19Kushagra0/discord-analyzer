68e087f6013881f5cb26e3fe0a817ae4d6ce1053
19Kushagra0 <19Kushagra0@gmail.com>
2026-05-30 05:49:35 +0530

    Kushagra dev (#9)
    * discord aouth added
    
    * next auth is working with firebase
    
    * add logout button
    
    * wire dashboard with discord aouth
    
    * coral added
    
    * coral added
    
    * header remodel
    
    * vercel error update

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -2,10 +2,10 @@ import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
   // Parse escaped newlines from the private key in .env.local
-  privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
 };
 
 let app;
